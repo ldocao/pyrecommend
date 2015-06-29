21afd8034200bd946bb50f0ca0c79e0b83364d26
ldocao <ldocao@gmail.com>
2015-06-29 16:27:34 +0200

    rating is a simple object

--- domain/atoms.py
+++ domain/atoms.py
@@ -2,6 +2,10 @@ import uuid
 
 
 
+
+
+
+
 class IDIsReadOnly(type):
     """Metaclass to prevent the alteration of ID class attribute"""
     
@@ -25,7 +29,8 @@ class User(object):
         self.points = 0 #experience points
         ##add here other caracteristics (country, premium status)
 
-
+    def __repr__(self):
+        return "User()"
 
     def add_points(self, additional_points):
         """Add points to his counter
@@ -54,14 +59,16 @@ class Movie(object):
         self.title = title
         self.producer = producer
         self.id = uuid.uuid4() #generate unique identifier UUID
-        self.tags = [] #list of strings
+        #self.tags = Tags() #list of strings
         
         try:
             self.year = int(year)
         except ValueError:
             self.year = "unknown"
             
+    
 
+            
     def __repr__(self):
         return "Movie()"
     
@@ -82,26 +89,33 @@ class Star(object):
         self.unit_name = "star"
 
 
-class Unity(object):
-    """Unit normalized to 1"""
+class Dimensionless(object):
+    """Dimensionless unit"""
 
     def __init__(self):
-        self.unit_name = "unity"
+        self.unit_name = "1"
 
 
 
 class Rating(object):
     """Rating of a movie by a given user"""
 
-    def __init__(self, movie_id, user_id, value=0):
+    def __init__(self, value=0, units=Star()):
         self.value = value
-        self.units = Star()
+        self.units = units
+
+        if value > self.units.maxvalue:
+            raise ValueError("Value is greater than max value in units of "+self.units.unit_name)
+
         self.normalized_value = (value - self.units.minvalue) / (
             self.units.maxvalue - self.units.minvalue) #rescale rating to [0,1]
-        
-
+                
+            
     def __str__(self):
-        return str(self.value)+"/"+str(self.units.maxvalue)
+        return str(round(self.value,1))+"/"+str(self.units.maxvalue)+" "+self.units.unit_name
+
+
+
 
 
         
@@ -126,3 +140,26 @@ class Score(object):
     def __init__(self):
         self.value = 0
         self.units = Dimensionless()
+
+
+
+
+class Tag(object):
+    """Tag for movie"""
+
+
+    def __init__(self, name):
+        self.name = name
+
+
+
+class Tags(object):
+    """List of Tags for movie"""
+
+    def __init__(self, list_tags):
+        self.names = [tag.name for tag in list_tags]
+
+
+
+
+user0=User("0000","0000")
--- test.py
+++ test.py
@@ -1,7 +1,29 @@
 ##PURPOSE: create a fake database to test algorithm
+import sys
+import os
+sys.path.append("/Users/ldocao/Documents/Professionnel/Data Science/S2DS/Utelly/pyrecommend/domain/")
+import atoms as at
 
 
-user1=User("Alfred", "Dupont")
-user2=User("Bob", "Dupont")
-user3=User("Charles", "Dupont")
-user4=User("David", "Dupont")
+
+user1 = at.User("Alfred", "Dupont")
+user2 = at.User("Bob", "Dupont")
+user3 = at.User("Charles", "Dupont")
+user4 = at.User("David", "Dupont")
+list_users = [user1, user2, user3, user4]
+n_users = len(list_users)
+
+
+movie1 = at.Movie("Dinosaurs")
+movie2 = at.Movie("Interstellar")
+movie3 = at.Movie("Vice Versa")
+movie4 = at.Movie("Barcelona")
+movie5 = at.Movie("Sex on the city")
+list_movies = [movie1, movie2, movie3, movie4, movie5]
+n_movies = len(list_movies)
+
+
+data = {}
+for user in list_users:
+    for movie in list_movies:
+        data[[user,movie]]=1
