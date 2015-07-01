5e6e92c43d5efb7616bfba2856d814a0b4ae7536
ldocao <ldocao@gmail.com>
2015-07-01 12:40:30 +0200

    cost function implemented, not tested

--- domain/regression.py
+++ domain/regression.py
@@ -1,7 +1,9 @@
 ##PURPOSE: minimize cost function to find best estimate for user profile features
-
+import sys
 import numpy as np
-import pdb
+
+sys.path.append("/Users/ldocao/Documents/Professionnel/Data Science/S2DS/Utelly/pyrecommend/domain/")
+from atoms import Rating
 
 # n_movies : number of movies
 # n_features : number of features for a movie (eg: genre)
@@ -18,7 +20,7 @@ import pdb
 
 
 
-def predicted_rating(user_affinity, movie_features):
+def _predicted_rating(user_affinity, movie_features):
     """Predicted rating for users on movies
 
     Parameters:
@@ -46,7 +48,7 @@ def predicted_rating(user_affinity, movie_features):
 
 
 
-def regularization_term(user_affinity, lambda_reg=1.):
+def _regularization_term(user_affinity, lambda_reg=1.):
     """Regularization term in the cost function
 
 
@@ -57,6 +59,10 @@ def regularization_term(user_affinity, lambda_reg=1.):
 
     lambda_reg: float
         Control parameter for regularization.
+
+    Return:
+    ------
+    float
     """
 
     regularization = sum([a.value**2 for a in user_affinity])
@@ -68,9 +74,8 @@ def regularization_term(user_affinity, lambda_reg=1.):
 
 
 
-def cost_function(user_affinity, movie_features, training_user_rating,
-                  lambda_reg=0.):
-    """Compute the cost function for a given set of user_affinity, movie_features and training_user_rating
+def _square_error(user_affinity, movie_features, training_user_rating):
+    """Compute the square error between the predicted rating and the training rating for a given user, and a given movie
 
     Parameters:
     ----------
@@ -89,16 +94,48 @@ def cost_function(user_affinity, movie_features, training_user_rating,
         Cost for the current parameters. Units are user_affinity.units**2
     """
 
-
-    cost = (predicted_rating(user_affinity,movie_features) - training_user_rating)
-    cost += regularization_term(user_affinity,lambda_reg=lambda_reg) #add regularization term
+    cost = (predicted_rating(user_affinity,movie_features) - training_user_rating).value**2
     
     return cost
 
 
 
-    
 
+
+def cost_function(user_affinity, movie_features, training_set,
+                  lambda_reg=0.):
+    """Compute the cost function for a given set of user_affinity, movie_features and training_user_rating for all movies.
+
+    Parameters:
+    ----------
+    user_affinity: list of Rating [n_users*n_features]
+        List of affinity for all features in a movie, for a given user. 
+
+    movie_features: list of Feature [n_movies*n_features]
+        List of features for a given movie
+
+    training_set: List of Rating [n_users*n_movies]
+        Rating from user for the given movie. 
+
+    Return:
+    ------
+    cost: Float
+        Cost for the current parameters. Units are user_affinity.units**2
+    """
+
+    
+    user_has_rated = [elem.value == None for elem in training_set]
+
+    total_cost = 0. #initialize total cost
+    total_regularization = 0 #initialize regularization term
+    for i in xrange(0,n_users):
+        for j in xrange(0,n_movies):
+            total_cost += _square_error(
+                user_affinity[i], movie_features[j],training_set[i,j])*0.5
+            
+        total_regularization += _regularization_term(user_affinity[i], lambda_reg=lambda_reg)
+        
+    return totalcost + total_regularization 
 
 
 def gradient_descent_update():
